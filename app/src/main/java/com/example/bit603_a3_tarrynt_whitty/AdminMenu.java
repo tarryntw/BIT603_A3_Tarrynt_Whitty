68a68a4adffffca2c3d0620b5a20c7633d3c3ae4
PoiDoe <tarryntw@gmail.com>
2022-05-28 15:56:16 +1200

    commit files

--- app/src/main/java/com/example/bit603_a3_tarrynt_whitty/AdminMenu.java
+++ app/src/main/java/com/example/bit603_a3_tarrynt_whitty/AdminMenu.java
@@ -3,6 +3,7 @@ package com.example.bit603_a3_tarrynt_whitty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AdminMenu extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class AdminMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_menu);
+
+
+        //get onscreen objects
+        Button viewUsers = findViewById(R.id.bVeiwUsers);
+        Button addUser = findViewById(R.id.bAddUser);
+        Button removeUser = findViewById(R.id.bRemoveUser);
+
     }
 }
