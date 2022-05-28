dba5bfd11a0f18331cf2d667af6943a490a0cbfe
PoiDoe <tarryntw@gmail.com>
2022-05-28 22:32:28 +1200

    Added add new user screen birthday can not be more then current date employ number must be number

--- app/src/main/java/com/example/bit603_a3_tarrynt_whitty/AdminAddUser.java
+++ app/src/main/java/com/example/bit603_a3_tarrynt_whitty/AdminAddUser.java
@@ -7,7 +7,7 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
+import android.widget.TextView;
 
 import java.util.Calendar;
 
@@ -27,6 +27,58 @@ public class AdminAddUser extends AppCompatActivity {
         dateButton = findViewById(R.id.datePickerButton);
         dateButton.setText(getTodaysDate());
 
+        //get onscreen objects
+
+        final TextView newUserName = findViewById(R.id.eTNewUserName);
+        final TextView newPassword = findViewById(R.id.eTNewPassword);
+        final TextView employeePhone = findViewById(R.id.eTphone);
+        final TextView employeeNumber = findViewById(R.id.eTEmployeeNumber);
+        final TextView address = findViewById(R.id.eTAddress);
+
+        Button addUserToDB = findViewById(R.id.bAddNewUser);
+
+        addUserToDB.setOnClickListener(view -> {
+            //get strings from text boxes
+            String usrname = newUserName.getText().toString();
+            String pssWord = newPassword.getText().toString();
+            String emplyPhone = employeePhone.getText().toString();
+            String emplyNumber = employeeNumber.getText().toString();
+            String emplyAddrss = address.getText().toString();
+
+            //check if strings are empty
+            boolean detailsentered = true;
+
+
+            if(usrname.isEmpty())
+                detailsentered = false;
+            if(pssWord.isEmpty())
+                detailsentered = false;
+            if(emplyPhone.isEmpty())
+                detailsentered = false;
+            if(emplyNumber.isEmpty())
+                detailsentered = false;
+            if(emplyAddrss.isEmpty())
+                detailsentered = false;
+            //if no details were entered, error
+            if (!detailsentered){
+                dialogNotComplete();
+            }
+
+            else{
+                //create new user
+                //grab database from MainActivity and add the new user
+                UserAccount newUser = new UserAccount(Integer.parseInt(emplyNumber), usrname, pssWord,useBirthDate, emplyPhone, emplyAddrss);
+                MainActivity.database.accountDao().addUser(newUser);
+                dialogComplete(usrname);
+                //clear all text fields
+                newUserName.setText("");
+                newPassword.setText("");
+                employeePhone.setText("");
+                employeeNumber.setText("");
+                address.setText("");
+            }
+        });
+
 
     }
 
@@ -41,15 +93,11 @@ public class AdminAddUser extends AppCompatActivity {
 
     private void initDatePicker() {
         //set some cal stuff
-        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month = month +1;
-                String date = makeDateString(day, month, year);
-                dateButton.setText(date);
-                useBirthDate = date;
-
-            }
+        DatePickerDialog.OnDateSetListener dateSetListener = (datePicker, year, month, day) -> {
+            month = month +1;
+            String date = makeDateString(day, month, year);
+            dateButton.setText(date);
+            useBirthDate = date;
         };
 
         //get to days date
@@ -61,6 +109,7 @@ public class AdminAddUser extends AppCompatActivity {
         int style = AlertDialog.THEME_HOLO_LIGHT;
 
         birthDatePicker = new DatePickerDialog(this, style,dateSetListener, year,month,day);
+        birthDatePicker.getDatePicker().setMaxDate(System.currentTimeMillis());
     }
 
     private String makeDateString(int day, int month, int year) {
@@ -103,5 +152,38 @@ public class AdminAddUser extends AppCompatActivity {
 
         birthDatePicker.show();
 
+    }
+
+    private void dialogNotComplete() {
+        // Instantiate
+        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(AdminAddUser.this);
+        // Set characteristics
+        builder.setMessage("not all information filled")
+                .setTitle("Please try again");
+        //add okay button
+        builder.setPositiveButton("OK", (dialog, id) -> {
+            // OK button was clicked
+        });
+        // Get AlertDialog
+        androidx.appcompat.app.AlertDialog dialog = builder.create();
+
+        // Show dialog
+        dialog.show();
+    }
+    private void dialogComplete(String user) {
+        // Instantiate
+        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(AdminAddUser.this);
+        // Set characteristics
+        builder.setMessage(user + " Added to database")
+                .setTitle("User Added");
+        //add okay button
+        builder.setPositiveButton("OK", (dialog, id) -> {
+            // OK button was clicked
+        });
+        // Get AlertDialog
+        androidx.appcompat.app.AlertDialog dialog = builder.create();
+
+        // Show dialog
+        dialog.show();
     }
 }
